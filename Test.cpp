714866c83648d5db142f166415018596a8f5b9a0
Andrews2114 <72770479+Andrews2114@users.noreply.github.com>
2023-12-01 10:07:35 -0600

    changes

--- Test.cpp
+++ Test.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
+
 using namespace std;
-int main () {
-int n, num;
-double sum;
-cout << "How may numbers are you going to type ";
-cin>> n;
 
-do {
-cout << "How may numbers are you going to type ";
-cin>> n;
-} while (n < 1 || n > 10);
+int main() {
+    int n, num;
+    double sum;
+    cout << "How may numbers are you going to type ";
+    cin >> n;
 
+
+    for (int c = 1; c < n; c++) {
+        cout << c << endl;
+    }
+
+    do {
+        cout << "How may numbers are you going to type ";
+        cin >> n;
+    } while (n < 1 || n > 10);
+}
